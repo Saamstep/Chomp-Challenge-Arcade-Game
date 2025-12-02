1769c93ce6010c635185fc24bb906d054e2cb4e8
Sam <samstep2012@gmail.com>
2025-12-02 11:37:41 -0800

    Baseline functional code

--- 460_final_code/460_final_code.ino
+++ 460_final_code/460_final_code.ino
@@ -1,7 +1,14 @@
 #include <Servo.h>
 #include "LedControl.h"
 
+// ------------------------------------
+// Penny Config
+// ------------------------------------
+#define PENNY_PIN 15
+
+// ------------------------------------
 // LED Display Config
+// ------------------------------------
 #define LC_DIN     12
 #define LC_CLK     11
 #define LC_CS      10
@@ -9,34 +16,41 @@
 
 LedControl lc = LedControl(LC_DIN, LC_CLK, LC_CS, LC_DEVICES);
 
-#define DISPLAY_LEFT 4
+#define DISPLAY_LEFT  4
 #define DISPLAY_RIGHT 0
 
-// IR Sensor Config (Ball-Score Detector) [Interrupt Pins]
+// ------------------------------------
+// IR Sensors (Ball Score Detectors)
+// ------------------------------------
 #define HIPPO1_SENSOR 2
 #define HIPPO2_SENSOR 3
 #define HIPPO3_SENSOR 21
-#define HIPPO4_SENSOR 20
-#define HIPPO5_SENSOR 19
 
-#define NUM_HIPPO_SENSORS 5
+#define NUM_HIPPO_SENSORS 3
+const int allHippoSensors[NUM_HIPPO_SENSORS] = {
+  HIPPO1_SENSOR, HIPPO2_SENSOR, HIPPO3_SENSOR
+};
 
-const int allHippoSensors[NUM_HIPPO_SENSORS] = {HIPPO1_SENSOR, HIPPO2_SENSOR, HIPPO3_SENSOR, HIPPO4_SENSOR, HIPPO5_SENSOR};
+// ------------------------------------
+// Servos (Hippo Mouths)
+// ------------------------------------
+#define HIPPO1_SERVO 7
+#define HIPPO2_SERVO 6
+#define HIPPO3_SERVO 5
 
-// Servo Config (Hippo Mouth)
-#define HIPPO1_SERVO 13
-#define HIPPO2_SERVO 12
-#define HIPPO3_SERVO 11
-#define HIPPO4_SERVO 10
-#define HIPPO5_SERVO 9
-
-#define NUM_HIPPO_SERVO 5
-
-const int allHippoServos[NUM_HIPPO_SERVO] = {HIPPO1_SERVO, HIPPO2_SERVO, HIPPO3_SERVO, HIPPO4_SERVO, HIPPO5_SERVO};
+#define NUM_HIPPO_SERVO 3
+const int allHippoServos[NUM_HIPPO_SERVO] = {
+  HIPPO1_SERVO, HIPPO2_SERVO, HIPPO3_SERVO
+};
 
 Servo hippoServo[NUM_HIPPO_SERVO];
 
+#define HIPPO_ANGLE_CLOSED 180
+#define HIPPO_ANGLE_OPEN   150
+
+// ------------------------------------
 // Game Config
+// ------------------------------------
 #define GAME_DURATION_S 30
 #define END_GAME_WAIT_S 5
 
@@ -44,47 +58,55 @@ enum state {
   IDLE,
   START,
   PLAYING,
-  END,
+  END
 };
 
-// Game Globals
-int score = 0;
-int timeLeft = 0;
-int pennyInserted = 0;
+// ------------------------------------
+// Globals
+// ------------------------------------
+volatile unsigned int score = 0;
+unsigned int timeLeft = 0;
+unsigned int pennyInserted = 0;
 state CS = IDLE;
 state NS = IDLE;
-long prevTime = 0;
+unsigned long prevTime = 0;
+unsigned int difficulty = 0;
 
+// ------------------------------------
+// ISR Debounce Tracking
+// ------------------------------------
+volatile unsigned long lastTriggerTime[NUM_HIPPO_SENSORS] = {0};
+const unsigned long SENSOR_DEBOUNCE_MS = 3000;
+
+// ====================================================================
 // Functions
+// ====================================================================
 
-// Display Control
-void DisplayReset()
-{
-  /* The MAX72XX is in power-saving mode on startup,
-  we have to do a wakeup call */
-  lc.shutdown(0,false);
-  /* Set the brightness to a medium values */
-  lc.setIntensity(0,8);
-  /* and clear the display */
+// Penny Input
+void PennyInit() {
+  pinMode(PENNY_PIN, INPUT_PULLUP);
+}
+
+// Display --------------------------------------------------------------
+
+void DisplayReset() {
+  lc.shutdown(0, false);
+  lc.setIntensity(0, 8);
   lc.clearDisplay(0);
 }
 
 void _DisplayWrite(int startDigit, int value) {
-  // startDigit = rightmost digit of the block
   lc.setDigit(0, startDigit,     value % 10, false);
-  lc.setDigit(0, startDigit +1, (value / 10) % 10, false);
-  lc.setDigit(0, startDigit +2, (value /100) % 10, false);
-  lc.setDigit(0, startDigit +3, (value /1000)% 10, false);
+  lc.setDigit(0, startDigit + 1, (value / 10) % 10, false);
+  lc.setDigit(0, startDigit + 2, (value / 100) % 10, false);
+  lc.setDigit(0, startDigit + 3, (value / 1000) % 10, false);
 }
 
-// 0: Left, 1: Right, 2: Both
-void DisplayWrite(int disp, int value)
-{
-  switch(disp)
-  {
+void DisplayWrite(int disp, int value) {
+  switch (disp) {
     case 0:
       _DisplayWrite(DISPLAY_LEFT, value);
-    break;
+      break;
     case 1:
       _DisplayWrite(DISPLAY_RIGHT, value);
       break;
@@ -95,135 +117,174 @@ void DisplayWrite(int disp, int value)
   }
 }
 
-void DisplayCountdown(int count)
-{
+void DisplayCountdown(int count) {
   DisplayWrite(1, 0);
-  DisplayWrite(0, 3);
-  delay(1000);
-  DisplayWrite(0, 2);
-  delay(1000);
-  DisplayWrite(0, 1);
-  delay(1000);
+  for (int i = count; i > 0; i--) {
+    DisplayWrite(0, i);
+    delay(1000);
+  }
 }
 
-void DisplayUpdateCounters(int timeLeft, int score);
+void DisplayUpdateCounters(int timeLeft, int score) {
+  DisplayWrite(0, timeLeft);
+  DisplayWrite(1, score);
+}
 
-// Servo Control
-void ServoSetup()
-{
-  for(int i = 0; i < NUM_HIPPO_SERVO; ++i)
-  {
+// Servos --------------------------------------------------------------
+
+void ServoSetup() {
+  for (int i = 0; i < NUM_HIPPO_SERVO; ++i) {
     hippoServo[i].attach(allHippoServos[i]);
-    hippoServo[i].write(0);
+    hippoServo[i].write(HIPPO_ANGLE_CLOSED);
+  }
+
+  randomSeed(analogRead(0));
+}
+
+void ServoReset() {
+  for (int i = 0; i < NUM_HIPPO_SERVO; ++i) {
+    hippoServo[i].write(HIPPO_ANGLE_CLOSED);
   }
 }
 
-void ServoReset()
-{
-  for(int i = 0; i < NUM_HIPPO_SERVO; ++i)
-  {
-    hippoServo[i].write(0);
-  }
-}
-
-void ServoSetAngle(int angle)
-{
-  for(int i = 0; i < NUM_HIPPO_SERVO; ++i)
-  {
+void ServoSetAngle(int angle) {
+  for (int i = 0; i < NUM_HIPPO_SERVO; ++i) {
     hippoServo[i].write(angle);
   }
 }
 
-// Sensor Reading
-void SensorISR_Normal()
-{
-// points ++
+void ServoOpenHippoMouth() {
+  int servoNum = random(0, NUM_HIPPO_SERVO);
+  ServoSetAngle(HIPPO_ANGLE_CLOSED);
+  hippoServo[servoNum].write(HIPPO_ANGLE_OPEN);
 }
 
-void SensorISR_Double()
-{
-// points += 2
-}
+// Sensor Interrupt Handlers -------------------------------------------
 
-void SensorInit()
-{
-  for(int i = 0; i < NUM_HIPPO_SENSORS; ++i)
-  {
-    if(i < 5)
-      attachInterrupt(digitalPinToInterrupt(allHippoSensors[i]), SensorISR_Normal, RISING);
-    else
-      attachInterrupt(digitalPinToInterrupt(allHippoSensors[i]), SensorISR_Double, RISING);
+void SensorISR_Normal() {
+  int sensorIndex = 0;  // HIPPO1 or HIPPO2 maps here
+  unsigned long now = millis();
+
+  if (now - lastTriggerTime[sensorIndex] >= SENSOR_DEBOUNCE_MS) {
+    score++;
+    lastTriggerTime[sensorIndex] = now;
   }
 }
 
-// Setup Function (Runs on Startup)
-void setup() {
-  // LCD Display Init
-  DisplayReset();
-  
-  // Servo Init
+void SensorISR_Normal2() {
+  int sensorIndex = 1;  // HIPPO2
+  unsigned long now = millis();
 
+  if (now - lastTriggerTime[sensorIndex] >= SENSOR_DEBOUNCE_MS) {
+    score++;
+    lastTriggerTime[sensorIndex] = now;
+  }
 }
+
+void SensorISR_Double() {
+  int sensorIndex = 2;  // HIPPO3 double score
+  unsigned long now = millis();
+
+  if (now - lastTriggerTime[sensorIndex] >= SENSOR_DEBOUNCE_MS) {
+    score += 2;
+    lastTriggerTime[sensorIndex] = now;
+  }
+}
+
+// Enable / Disable Interrupts -----------------------------------------
+
+void EnableSensorInterrupts() {
+  attachInterrupt(digitalPinToInterrupt(allHippoSensors[0]), SensorISR_Normal, RISING);
+  attachInterrupt(digitalPinToInterrupt(allHippoSensors[1]), SensorISR_Normal2, RISING);
+  attachInterrupt(digitalPinToInterrupt(allHippoSensors[2]), SensorISR_Double, RISING);
+}
+
+void DisableSensorInterrupts() {
+  for (int i = 0; i < NUM_HIPPO_SENSORS; i++) {
+    detachInterrupt(digitalPinToInterrupt(allHippoSensors[i]));
+  }
+}
+
+// ====================================================================
+// Setup
+// ====================================================================
+
+void setup() {
+  DisplayReset();
+  ServoSetup();
+  PennyInit();
+  DisableSensorInterrupts();  // start safe
+}
+
+// ====================================================================
+// Main Loop
+// ====================================================================
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //DisplayCountdown(0);
-  long time = millis();
-  long timeElapsed = time - prevTime;
+  unsigned long timeNow = millis();
+  unsigned long timeElapsed = timeNow - prevTime;
 
-  switch(CS)
-  {
+  // -------------------------
+  // STATE TRANSITION LOGIC
+  //-------------------------
+
+  switch (CS) {
     case IDLE:
-      if(pennyInserted == HIGH)
-      {
-        NS = START;
-      } else
-      {
-        NS = IDLE;
-      }
+      pennyInserted = digitalRead(PENNY_PIN);
+      NS = (pennyInserted == HIGH) ? START : IDLE;
       break;
+
     case START:
-      if(timeLeft == GAME_DURATION_S)
-      {
-        NS = PLAYING;
-      } else if(timeLeft == 0)
-      {
-        NS = START;
-      }
+      NS = START;
       break;
+
     case PLAYING:
-      if(timeLeft < GAME_DURATION_S && timeLeft > 0)
-      {
-        NS = PLAYING;
-      } else
-      {
-        NS = END;
-      }
+      NS = (timeLeft > 0) ? PLAYING : END;
       break;
+
     case END:
-      if(timeElapsed >= END_GAME_WAIT_S*1000)
-      {
-        NS = IDLE;
-      } else
-      {
-        NS = END;
-      }
+      NS = (timeElapsed >= END_GAME_WAIT_S * 1000) ? IDLE : END;
       break;
   }
 
-    switch(CS)
-  {
+  // -------------------------
+  // STATE ACTIONS
+  //-------------------------
+
+  switch (CS) {
+
     case IDLE:
-
+      DisableSensorInterrupts();
       break;
+
     case START:
-
+      DisableSensorInterrupts();
+      DisplayCountdown(5);
+      score = 0;
+      timeLeft = GAME_DURATION_S;
+      prevTime = millis();
+      NS = PLAYING;
       break;
+
     case PLAYING:
+      EnableSensorInterrupts();
 
+      if (timeElapsed >= 1000) {
+        ServoOpenHippoMouth();
+        timeLeft--;
+        prevTime = millis();
+        DisplayUpdateCounters(timeLeft, score);
+      }
       break;
-    case END:
 
+    case END:
+      DisableSensorInterrupts();
+
+      if (timeElapsed == 0) {
+        DisplayWrite(0, 0);
+        DisplayWrite(1, score);
+        prevTime = millis();
+      }
       break;
   }
 
